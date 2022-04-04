d822a05fe43215192de5b780c7f059957524716c
Nemezisp <p.jarmula5@gmail.com>
2022-04-04 17:56:25 +0200

    check error

--- controllers/register.js
+++ controllers/register.js
@@ -11,7 +11,8 @@ const handleRegister = (req, res, db, bcrypt, saltRounds) => {
         trx.insert({
             hash: hash,
             email: email
-        }).into('login')
+        })
+        .into('login')
         .returning('email')
         .then(loginEmail => {
             return trx('users')
@@ -24,12 +25,11 @@ const handleRegister = (req, res, db, bcrypt, saltRounds) => {
             .then(user => {
                 res.json(user[0])
             })
-
         })
         .then(trx.commit)
         .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register'))
+    .catch(err => res.status(400).json(err))
 }
 
 module.exports = {
