053a6306f215d502827196a7831fa5babe8b6427
Nemezisp <p.jarmula5@gmail.com>
2022-04-04 18:10:05 +0200

    api key to heroku

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai')
 
 const app = new Clarifai.App({
-    apiKey: '157d356ed7c44b7facfe0ca4c93eac59'
+    apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
