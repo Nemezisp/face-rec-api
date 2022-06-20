27ab65c19982fe4a65c07be7d7ead387a959fa0e
Nemezisp <p.jarmula5@gmail.com>
2022-06-20 10:04:32 +0200

    chaning redis provider in Heroku

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -1,75 +1,84 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-if (process.env.REDISTOGO_URL) {
-  var rtg = require("url").parse(process.env.REDISTOGO_URL);
-  var redisClient = require("redis").createClient(rtg.port, rtg.hostname);
-  redisClient.auth(rtg.auth.split(":")[1]);
+if (process.env.REDIS_URL) {
+  var redisClient = require("redis").createClient({
+    url: process.env.REDIS_URL,
+  });
 } else {
   var redisClient = require("redis").createClient();
 }
 
 const handleSignIn = (req, res, db, bcrypt) => {
-  const {email, password} = req.body
+  const { email, password } = req.body;
 
   if (!email || !password) {
-      return Promise.reject('incorrect form submission')
+    return Promise.reject("incorrect form submission");
   }
 
-  return db.select('email', 'hash').from('login')
-    .where('email', '=', email)
-    .then(data => {
-        const isValid = bcrypt.compareSync(password, data[0].hash);
-        if (isValid) {
-            return db.select('*').from('users')
-              .where('email', '=', email)
-              .then(user => user[0])
-              .catch(err =>  Promise.reject('unable to get user'))
-        } else {
-            Promise.reject('wrong credentials')
-        }
+  return db
+    .select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then((user) => user[0])
+          .catch((err) => Promise.reject("unable to get user"));
+      } else {
+        Promise.reject("wrong credentials");
+      }
     })
-    .catch(err => Promise.reject('wrong credentials'))
-}
+    .catch((err) => Promise.reject("wrong credentials"));
+};
 
 const getAuthTokenId = (req, res) => {
   const { authorization } = req.headers;
   return redisClient.get(authorization, (err, reply) => {
     if (err || !reply) {
-      return res.status(400).json('Unauthorized')
+      return res.status(400).json("Unauthorized");
     }
-    return res.json({id: reply})
-  })
-}
+    return res.json({ id: reply });
+  });
+};
 
 const signToken = (email) => {
-  const jwtPayload = { email }
-  return jwt.sign(jwtPayload, process.env.JWTSECRET, {expiresIn: '2 days'});
-}
+  const jwtPayload = { email };
+  return jwt.sign(jwtPayload, process.env.JWTSECRET, { expiresIn: "2 days" });
+};
 
 const setToken = (token, id) => {
-  return Promise.resolve(redisClient.set(token, id))
-}
+  return Promise.resolve(redisClient.set(token, id));
+};
 
 const createSession = (user) => {
   const { email, id } = user;
   const token = signToken(email);
   return setToken(token, id)
-    .then(() => { return {success: 'true', userId: id, token}})
-    .catch(console.log)
-}
+    .then(() => {
+      return { success: "true", userId: id, token };
+    })
+    .catch(console.log);
+};
 
 const signInAuthentication = (req, res, db, bcrypt) => {
   const { authorization } = req.headers;
-  return authorization ? getAuthTokenId(req, res) : 
-    handleSignIn(req, res, db, bcrypt)
-      .then(data => {
-        return data.id && data.email ? createSession(data) : Promise.reject('Error')
-      })
-      .then(session => res.json(session))
-      .catch(err => res.status(400))
-}
+  return authorization
+    ? getAuthTokenId(req, res)
+    : handleSignIn(req, res, db, bcrypt)
+        .then((data) => {
+          return data.id && data.email
+            ? createSession(data)
+            : Promise.reject("Error");
+        })
+        .then((session) => res.json(session))
+        .catch((err) => res.status(400));
+};
 
 module.exports = {
-    signInAuthentication,
-    redisClient
-}
+  signInAuthentication,
+  redisClient,
+};
--- server.js
+++ server.js
@@ -1,48 +1,82 @@
-const express = require('express')
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const multer = require('multer')
-const upload = multer()
+const multer = require("multer");
+const upload = multer();
 
-require('dotenv').config();
+require("dotenv").config();
 
-const register = require('./controllers/register')
-const signIn = require('./controllers/signIn')
-const image = require('./controllers/image')
-const profile = require('./controllers/profile')
-const auth = require('./controllers/authorization')
-const s3url = require('./controllers/S3url')
+const register = require("./controllers/register");
+const signIn = require("./controllers/signIn");
+const image = require("./controllers/image");
+const profile = require("./controllers/profile");
+const auth = require("./controllers/authorization");
+const s3url = require("./controllers/S3url");
 
-const cors = require('cors');
+const cors = require("cors");
 
+const db = require("knex")({
+  //Heroku database connection
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
 
-const db = require('knex')({ //Heroku database connection
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    }
-})
-
-const app = express()
+const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {res.send('success')})
-app.post('/signin', (req, res) => {signIn.signInAuthentication(req, res, db, bcrypt)})
-app.post('/register', (req, res) => {register.registerAuthentication(req, res, db, bcrypt, saltRounds)})
-app.get('/profile/:id', auth.requireAuth, (req, res) => {profile.handleProfileGet(req, res, db)})
-app.post('/profile/:id', auth.requireAuth, (req, res) => {profile.handleProfileNameUpdate(req, res, db)})
-app.post('/profilepicture/:id', auth.requireAuth, (req, res) => {profile.handleProfilePictureUpdate(req, res, db)})
-app.put('/image', auth.requireAuth, (req, res) => {image.handleImage(req, res, db)})
-app.post('/imageurl', auth.requireAuth, (req, res) => {image.handleApiCall(req, res)})
-app.post('/localimage', upload.single('image'), auth.requireAuth, (req, res) => {image.handleLocalApiCall(req, res)})
-app.get('/S3url', auth.requireAuth, (req, res) => {s3url.getS3url(req, res)})
+app.get("/", (req, res) => {
+  res.send("success");
+});
+
+app.post("/signin", (req, res) => {
+  signIn.signInAuthentication(req, res, db, bcrypt);
+});
+
+app.post("/register", (req, res) => {
+  register.registerAuthentication(req, res, db, bcrypt, saltRounds);
+});
+
+app.get("/profile/:id", auth.requireAuth, (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.post("/profile/:id", auth.requireAuth, (req, res) => {
+  profile.handleProfileNameUpdate(req, res, db);
+});
+
+app.post("/profilepicture/:id", auth.requireAuth, (req, res) => {
+  profile.handleProfilePictureUpdate(req, res, db);
+});
+
+app.put("/image", auth.requireAuth, (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", auth.requireAuth, (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.post(
+  "/localimage",
+  upload.single("image"),
+  auth.requireAuth,
+  (req, res) => {
+    image.handleLocalApiCall(req, res);
+  }
+);
+
+app.get("/S3url", auth.requireAuth, (req, res) => {
+  s3url.getS3url(req, res);
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT || 3000}`)
-})
+  console.log(`app is running on port ${process.env.PORT || 3000}`);
+});
